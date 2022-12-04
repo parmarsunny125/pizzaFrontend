f43e5fcd0ab12fdc1735da675c65a228b8fad698
parmarsunny125 <parmarsunny125@gmail.com>
2022-12-05 00:50:58 +0530

    Sunny

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -16,7 +16,7 @@ export const registerUser = (user) => async (dispatch) => {
 export const loginUser = (user) => async (dispatch) => {
   dispatch({ type: "USER_LOGIN_REQUEST" });
   try {
-    const response = await axios.post("/api/users/login", user);
+    const response = await axios.post(`${API_URL}/api/users/login`, user);
     // console.log(response);
     dispatch({ type: "USER_LOGIN_SUCCESS", payload: response.data });
     localStorage.setItem("currentUser", JSON.stringify(response.data));
